deb3258af6140a4a8a0557ca65a9413cf211f8d3
caralin3 <caralin3@gmail.com>
2018-09-06 11:28:15 -0400

    Implemented intial header style and added links; edited package.json

---
+++ vue-todo-list/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    <h1 class="header_title">Todo</h1>
+    <div class="header_nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/board">Board</router-link>
+      <router-link to="/list">List</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+export default class Header extends Vue {}
+</script>
+
+<style lang="less">
+@import '../less/index.less';
+</style>
