99f5ad162cb4dba8598badf1d20f5f75cc9539e9
caralin3 <caralin3@gmail.com>
2018-09-14 13:31:47 -0400

    Refractored add project dialog and fixed toggle

---
+++ vue-todo-list/src/components/AddProjectDialog.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="addProject">
+    <Dialog title="Create Project" :toggleDialog="dismissDialog">
+      <Form buttonText="Add Project" :toggleDialog="dismissDialog" :submit="onSubmitForm">
+        <div class="addProject_dialog">
+          <span class="addProject_creatorLabel" :creator="creator">
+            Creator
+          </span>
+          <span class="addProject_username">{{ creator | name }}</span>
+          <TextInput :class="'addProject_textInput'" label="Project Name" placeholder="" v-model="title" :onBlur="() => null" :onFocus="() => null" />
+          <TextAreaInput :class="'addProject_textAreaInput'" label="Project Description" placeholder="" v-model="description" :onBlur="() => null" :onFocus="() => null" />
+          <DateInput :class="'addProject_dateInput'" label="Start Date" v-model="startDate" :onBlur="() => null" :onFocus="() => null" />
+          <DateInput :class="'addProject_dateInput'" label="End Date" v-model="endDate" :onBlur="() => null" :onFocus="() => null" />
+          <SelectInput :class="'addProject_selectStatus'" label="Status" v-model="status" :options="statusOptions" :onBlur="() => null" :onFocus="() => null" />
+          <TextInput :class="'addProject_textInput'" label="Version" placeholder="" v-model="version" :onBlur="() => null" :onFocus="() => null" />
+        </div>
+      </Form>
+    </Dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { mapActions } from 'vuex';
+import DateInput from '@/components/DateInput.vue';
+import Dialog from '@/components/Dialog.vue';
+import SelectInput from '@/components/SelectInput.vue';
+import Form from '@/components/Form.vue';
+import ProjectList from '@/components/ProjectList.vue';
+import ProjectsHeader from '@/components/ProjectsHeader.vue';
+import TextAreaInput from '@/components/TextAreaInput.vue';
+import TextInput from '@/components/TextInput.vue';
+import { User1 } from '@/store/state';
+import { Project, User, statusType } from '@/types';
+import { statusOptions } from '@/utils/constants';
+import { uid } from '@/utils/guid';
+
+@Component({
+  components: {
+    DateInput,
+    Dialog,
+    SelectInput,
+    Form,
+    ProjectsHeader,
+    ProjectList,
+    TextAreaInput,
+    TextInput,
+  },
+  props: {
+    toggleDialog: Function,
+  },
+  data: () => ({
+    creator: User1 as User,
+    description: '',
+    endDate: '',
+    title: '',
+    startDate: new Date(),
+    status: 'todo' as statusType,
+    version: String,
+    statusOptions,
+  }),
+  methods: {
+    ...mapActions('projects', [
+      'addProject',
+    ]),
+    dismissDialog(this: any) {
+      this.toggleDialog();
+    },
+    onSubmitForm(this: any) {
+      const newProj: Project = {
+        creator: this.creator,
+        description: this.description,
+        endDate: this.endDate,
+        features: [],
+        id: uid(8),
+        startDate: this.startDate,
+        status: this.status,
+        title: this.title,
+        updatedDate: this.startDate,
+        versions: [this.version],
+      };
+      this.addProject(newProj);
+    },
+  },
+})
+export default class Projects extends Vue {}
+</script>
--- vue-todo-list/src/views/Projects.vue
+++ vue-todo-list/src/views/Projects.vue
@@ -2,28 +2,14 @@
   <div class="projects">
     <ProjectsHeader :toggleDialog="toggleDialog" />
     <ProjectList />
-    <Dialog title="Create Project" :toggleDialog="toggleDialog" v-if="show">
-      <Form buttonText="Add Project" :toggleDialog="toggleDialog" :submit="onSubmitForm">
-        <div class="projects_dialog">
-        <span class="projects_creatorLabel" :creator="creator">
-          Creator
-        </span>
-        <span class="projects_username">{{ creator | name }}</span>
-        <TextInput :class="'projects_textInput'" label="Project Name" placeholder="" v-model="title" />
-        <TextAreaInput :class="'projects_textAreaInput'" label="Project Description" placeholder="" v-model="description" />
-        <DateInput :class="'projects_dateInput'" label="Start Date" v-model="startDate" />
-        <DateInput :class="'projects_dateInput'" label="End Date" v-model="endDate" />
-        <SelectInput :class="'projects_selectStatus'" label="Status" v-model="status" :options="statusOptions" />
-        <TextInput :class="'projects_textInput'" label="Version" placeholder="" v-model="version" />
-        </div>
-      </Form>
-    </Dialog>
+    <AddProjectDialog v-if="show" :toggleDialog="toggleDialog" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { mapActions } from 'vuex';
+import AddProjectDialog from '@/components/AddProjectDialog.vue';
 import DateInput from '@/components/DateInput.vue';
 import Dialog from '@/components/Dialog.vue';
 import SelectInput from '@/components/SelectInput.vue';
@@ -39,6 +25,7 @@ import { uid } from '@/utils/guid';
 
 @Component({
   components: {
+    AddProjectDialog,
     DateInput,
     Dialog,
     SelectInput,
@@ -49,37 +36,11 @@ import { uid } from '@/utils/guid';
     TextInput,
   },
   data: () => ({
-    creator: User1 as User,
-    description: '',
-    endDate: '',
-    title: '',
-    startDate: new Date(),
-    status: 'todo' as statusType,
     show: false,
-    version: String,
-    statusOptions,
   }),
   methods: {
-    ...mapActions('projects', [
-      'addProject',
-    ]),
     toggleDialog(this: any) {
       this.show = !this.show;
-    },
-    onSubmitForm(this: any) {
-      const newProj: Project = {
-        creator: this.creator,
-        description: this.description,
-        endDate: this.endDate,
-        features: [],
-        id: uid(8),
-        startDate: this.startDate,
-        status: this.status,
-        title: this.title,
-        updatedDate: this.startDate,
-        versions: [this.version],
-      };
-      this.addProject(newProj);
     },
   },
 })
