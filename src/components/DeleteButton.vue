cde4f23078e3dcfe981b6dfbb31abadfe5f8981e
caralin3 <caralin3@gmail.com>
2018-10-11 16:57:37 -0400

    Implemented delete links

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <i class="deleteButton far fa-trash-alt" @click="onClick" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'DeleteButton',
+
+  props: {
+    onClick: {
+      type: Function,
+    },
+  },
+});
+</script>
+
+<style lang="less" scoped>
+@import '../less/variables.less';
+
+.deleteButton {
+  transition: 0.2s ease;
+
+  &:hover {
+    color: @medium-sea-green;
+    cursor: pointer;
+  }
+}
+</style>
